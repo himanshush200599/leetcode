422829b03ab8ab258e6ccefbc8fef8e259caad44
Manishka Gautam <35163295+manishkaa@users.noreply.github.com>
2019-10-24 21:30:13 +0530

    Create change

--- verifyaliendic.py
+++ verifyaliendic.py
@@ -5,17 +5,17 @@ class Solution(object):
         :type order: str
         :rtype: bool
         """
-        d={}
+        dic={}
         j=0
         for i in order:
-           d[i]=j 
+           dic[i]=j 
            j+=1
         for i in range(len(words)-1):
             w1=words[i]
             w2=words[i+1]
             for k in range(min(len(w1),len(w2))):
                 if w1[k]!=w2[k]:
-                    if d[w1[k]]>d[w2[k]]:
+                    if dic[w1[k]]>dic[w2[k]]:
                         return False
                     break
             else:
